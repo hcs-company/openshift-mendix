57c544bd7e316d296687e8896ae789c52f1abddf
Marcel Kerker <marcel.kerker@outlook.com>
2019-05-28 11:24:03 +0200

    Mendix on Openshift

--- create_mendix.sh
+++ create_mendix.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 set -x
 
 PROJECT=mendix-on-openshift
 APP_NAME=mendix-app
 
-DATEBASE_NAME=mendix
-DATEBASE_USER=mendix
-DATEBASE_PASSWORD=mendix
-DATABASE_SERVICE_NAME=postgresql-mendix
+DATEBASE_NAME=mendix-db0
+DATEBASE_USER=mendix-user
+DATEBASE_PASSWORD=mendix-pwd
+DATABASE_SERVICE_NAME=mendix-db
 DATEBASE_ENDPOINT=postgres://${DATEBASE_USER}:${DATEBASE_PASSWORD}@${DATABASE_SERVICE_NAME}.${PROJECT}.svc.cluster.local:5432/${DATEBASE_NAME}
 ADMIN_PASSWORD=P@ssw0rd
 
