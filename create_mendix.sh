47f70c08e82e7f5de632c8848267807e28bcb054
Marcel Kerker <marcel.kerker@outlook.com>
2019-06-06 09:29:52 +0200

    Last changes

--- create_mendix.sh
+++ create_mendix.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -x
 
-PROJECT=mendix-on-openshift-test
+PROJECT=mendix-on-openshift
+MENDIX_APP_DIR=mendix-sample-app
 APP_NAME=mendix-app
 ADMIN_PASSWORD=P@ssw0rd
 
@@ -11,12 +12,26 @@ DATEBASE_PASSWORD=mendix-pwd
 DATABASE_SERVICE_NAME=mendix-db
 DATEBASE_ENDPOINT=postgres://${DATEBASE_USER}:${DATEBASE_PASSWORD}@${DATABASE_SERVICE_NAME}.${PROJECT}.svc.cluster.local:5432/${DATEBASE_NAME}
 
+echo "Create new project: ${PROJECT}" 
 oc new-project ${PROJECT}
 
-oc new-build https://github.com/hcs-company/openshift-mendix --name=${APP_NAME}
+echo "Build Mendix image with name: ${APP_NAME}"
+oc new-build . --strategy=docker --build-arg=BUILD_PATH=${MENDIX_APP_DIR} --name=${APP_NAME}
 
+echo "Create PostegreSql Database"
 oc new-app --template=openshift/postgresql-persistent --param=DATABASE_SERVICE_NAME=${DATABASE_SERVICE_NAME} --param=POSTGRESQL_USER=${DATEBASE_USER} --param=POSTGRESQL_PASSWORD=${DATEBASE_PASSWORD} --param=POSTGRESQL_DATABASE=${DATEBASE_NAME}
 
+echo "Create a secret for the MxAdmin password and the datebase endpoint"
 oc create secret generic mendix-app-secrets --from-literal=admin-password=${ADMIN_PASSWORD} --from-literal=db-endpoint=${DATEBASE_ENDPOINT}
 
-oc create -f https://raw.githubusercontent.com/hcs-company/openshift-mendix/master/${APP_NAME}.yaml
+echo "Create a statefuleset of the application"
+oc create -f ${APP_NAME}.yaml
+
+echo "Set a deployment trigger if the image changes"
+oc set triggers statefulset/mendix-ocp-stateful --from-image=${PROJECT}/${APP_NAME}:latest -c mendix-app
+
+echo "Go to URL: https://mendix.ocp.hcs-company.com"
+echo ""
+echo "UserID: MxAdmin"
+echo "Password: ${ADMIN_PASSWORD}"
+
